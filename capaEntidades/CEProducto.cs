adcd20bd231392e7e1388011ad4194a63038a3f7
Prado2546 <30426028+prado2546@users.noreply.github.com>
2017-07-25 00:14:12 -0600

    Agregado set and get
    Se agrego get and set en tabla producto

--- capaEntidades/CEProducto.cs
+++ capaEntidades/CEProducto.cs
@@ -8,5 +8,13 @@ namespace capaEntidades
 {
     class CEProducto
     {
+
+        //Se agregar los setters and getter de la tabla producto...
+
+        public int IdProducto { set; get; }
+        public string Nombre { set; get; }
+        public decimal Precio { set; get; }
+        public int IdTipoProducto { set; get; }
+
     }
 }
