80aa1cf65d955e7e0cb412c23550ec7d30b3a121
Prado2546 <30426028+prado2546@users.noreply.github.com>
2017-07-25 00:22:47 -0600

    Agrege set and get a empleado
    Empleado con set and get

--- capaEntidades/CEEmpleado.cs
+++ capaEntidades/CEEmpleado.cs
@@ -8,5 +8,16 @@ namespace capaEntidades
 {
     class CEEmpleado
     {
+        //Agregado set and get de entidad Empleado...
+
+        public int IdEmpleado { set; get; }
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public string Telefono { set; get; }
+        public string Correo { set; get; }
+        public string Direccion { set; get; }
+        public int IdSexo { set; get; }
+        public int IdTipoEmpleado { set; get; }
+
     }
 }
