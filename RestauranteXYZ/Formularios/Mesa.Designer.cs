7bacd344a9e808f5d2cd7c6f2d6f88498d9bdb48
DeadAimEXT <jrangulod@gmail.com>
2017-07-27 03:05:27 -0600

    Coreccion de errores de merge

--- RestauranteXYZ/Formularios/Mesa.Designer.cs
+++ RestauranteXYZ/Formularios/Mesa.Designer.cs
@@ -48,7 +48,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbMesa9)).BeginInit();
             this.SuspendLayout();
             // 
-<<<<<<< HEAD
+
             // pbMesa1
             // 
             this.pbMesa1.BackColor = System.Drawing.Color.Transparent;
@@ -138,109 +138,7 @@
             this.pbMesa9.Size = new System.Drawing.Size(90, 128);
             this.pbMesa9.TabIndex = 8;
             this.pbMesa9.TabStop = false;
-=======
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(183, 70);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(13, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "5";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(78, 178);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(78, 396);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(13, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "4";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(78, 287);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "3";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Location = new System.Drawing.Point(78, 70);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "1";
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = global::RestauranteXYZ.Properties.Resources.mesa;
-            this.pictureBox5.Location = new System.Drawing.Point(35, 349);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(100, 103);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 4;
-            this.pictureBox5.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::RestauranteXYZ.Properties.Resources.mesa;
-            this.pictureBox4.Location = new System.Drawing.Point(35, 240);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 103);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::RestauranteXYZ.Properties.Resources.mesa;
-            this.pictureBox3.Location = new System.Drawing.Point(141, 22);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 103);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::RestauranteXYZ.Properties.Resources.mesa;
-            this.pictureBox2.Location = new System.Drawing.Point(35, 131);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 103);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::RestauranteXYZ.Properties.Resources.mesa;
-            this.pictureBox1.Location = new System.Drawing.Point(35, 22);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 103);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
->>>>>>> 7cd728834a8248c3d856d7996a97373babdca09d
+
             // 
             // Mesa
             // 
