68367eb91c3a96df34b5a6b4b763aea89a04edac
DeadAimEXT <jrangulod@gmail.com>
2017-08-03 14:28:58 -0600

    mini y max

--- RestauranteXYZ/Formularios/frmMenu.Designer.cs
+++ RestauranteXYZ/Formularios/frmMenu.Designer.cs
@@ -29,22 +29,22 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pbOrden = new System.Windows.Forms.PictureBox();
+            this.pbEmpleado = new System.Windows.Forms.PictureBox();
+            this.pbAudit = new System.Windows.Forms.PictureBox();
+            this.pbFactura = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.pbReportes = new System.Windows.Forms.PictureBox();
-            this.pbOrden = new System.Windows.Forms.PictureBox();
-            this.pbEmpleado = new System.Windows.Forms.PictureBox();
-            this.pbAudit = new System.Windows.Forms.PictureBox();
-            this.pbFactura = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbReportes)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbOrden)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbEmpleado)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbAudit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbFactura)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbReportes)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -70,6 +70,54 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(593, 552);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pbOrden
+            // 
+            this.pbOrden.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbOrden.Image = global::RestauranteXYZ.Properties.Resources.menu;
+            this.pbOrden.Location = new System.Drawing.Point(333, 43);
+            this.pbOrden.Name = "pbOrden";
+            this.pbOrden.Size = new System.Drawing.Size(225, 214);
+            this.pbOrden.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOrden.TabIndex = 1;
+            this.pbOrden.TabStop = false;
+            this.pbOrden.Click += new System.EventHandler(this.pbOrden_Click);
+            // 
+            // pbEmpleado
+            // 
+            this.pbEmpleado.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbEmpleado.Image = global::RestauranteXYZ.Properties.Resources.waiter;
+            this.pbEmpleado.Location = new System.Drawing.Point(36, 43);
+            this.pbEmpleado.Name = "pbEmpleado";
+            this.pbEmpleado.Size = new System.Drawing.Size(226, 214);
+            this.pbEmpleado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbEmpleado.TabIndex = 0;
+            this.pbEmpleado.TabStop = false;
+            this.pbEmpleado.Click += new System.EventHandler(this.pbEmpleado_Click);
+            // 
+            // pbAudit
+            // 
+            this.pbAudit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbAudit.Image = global::RestauranteXYZ.Properties.Resources.restaurant;
+            this.pbAudit.Location = new System.Drawing.Point(36, 317);
+            this.pbAudit.Name = "pbAudit";
+            this.pbAudit.Size = new System.Drawing.Size(226, 214);
+            this.pbAudit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAudit.TabIndex = 2;
+            this.pbAudit.TabStop = false;
+            this.pbAudit.Click += new System.EventHandler(this.pbAudit_Click);
+            // 
+            // pbFactura
+            // 
+            this.pbFactura.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbFactura.Image = global::RestauranteXYZ.Properties.Resources.invoice;
+            this.pbFactura.Location = new System.Drawing.Point(333, 317);
+            this.pbFactura.Name = "pbFactura";
+            this.pbFactura.Size = new System.Drawing.Size(226, 214);
+            this.pbFactura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFactura.TabIndex = 3;
+            this.pbFactura.TabStop = false;
+            this.pbFactura.Click += new System.EventHandler(this.pbFactura_Click);
             // 
             // label1
             // 
@@ -143,62 +191,6 @@
             this.pbReportes.TabStop = false;
             this.pbReportes.Click += new System.EventHandler(this.pbReportes_Click);
             // 
-            // pbOrden
-            // 
-            this.pbOrden.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pbOrden.Image = global::RestauranteXYZ.Properties.Resources.menu;
-            this.pbOrden.Location = new System.Drawing.Point(333, 43);
-            this.pbOrden.Name = "pbOrden";
-            this.pbOrden.Size = new System.Drawing.Size(225, 214);
-            this.pbOrden.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbOrden.TabIndex = 1;
-            this.pbOrden.TabStop = false;
-            this.pbOrden.Click += new System.EventHandler(this.pbOrden_Click);
-            this.pbOrden.MouseEnter += new System.EventHandler(this.pbOrden_MouseEnter);
-            this.pbOrden.MouseLeave += new System.EventHandler(this.pbOrden_MouseLeave);
-            // 
-            // pbEmpleado
-            // 
-            this.pbEmpleado.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pbEmpleado.Image = global::RestauranteXYZ.Properties.Resources.waiter;
-            this.pbEmpleado.Location = new System.Drawing.Point(36, 43);
-            this.pbEmpleado.Name = "pbEmpleado";
-            this.pbEmpleado.Size = new System.Drawing.Size(226, 214);
-            this.pbEmpleado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbEmpleado.TabIndex = 0;
-            this.pbEmpleado.TabStop = false;
-            this.pbEmpleado.Click += new System.EventHandler(this.pbEmpleado_Click);
-            this.pbEmpleado.MouseEnter += new System.EventHandler(this.pbEmpleado_MouseEnter);
-            this.pbEmpleado.MouseLeave += new System.EventHandler(this.pbEmpleado_MouseLeave);
-            // 
-            // pbAudit
-            // 
-            this.pbAudit.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pbAudit.Image = global::RestauranteXYZ.Properties.Resources.restaurant;
-            this.pbAudit.Location = new System.Drawing.Point(36, 317);
-            this.pbAudit.Name = "pbAudit";
-            this.pbAudit.Size = new System.Drawing.Size(226, 214);
-            this.pbAudit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbAudit.TabIndex = 2;
-            this.pbAudit.TabStop = false;
-            this.pbAudit.Click += new System.EventHandler(this.pbAudit_Click);
-            this.pbAudit.MouseEnter += new System.EventHandler(this.pbAudit_MouseEnter);
-            this.pbAudit.MouseLeave += new System.EventHandler(this.pbAudit_MouseLeave);
-            // 
-            // pbFactura
-            // 
-            this.pbFactura.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pbFactura.Image = global::RestauranteXYZ.Properties.Resources.invoice;
-            this.pbFactura.Location = new System.Drawing.Point(333, 317);
-            this.pbFactura.Name = "pbFactura";
-            this.pbFactura.Size = new System.Drawing.Size(226, 214);
-            this.pbFactura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbFactura.TabIndex = 3;
-            this.pbFactura.TabStop = false;
-            this.pbFactura.Click += new System.EventHandler(this.pbFactura_Click);
-            this.pbFactura.MouseEnter += new System.EventHandler(this.pbFactura_MouseEnter);
-            this.pbFactura.MouseLeave += new System.EventHandler(this.pbFactura_MouseLeave);
-            // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -208,16 +200,18 @@
             this.Controls.Add(this.pbReportes);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbReportes)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbOrden)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbEmpleado)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbAudit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbFactura)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbReportes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
